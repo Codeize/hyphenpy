04aed0d50b10a04a24fef0ab885160218aad3e3b
Codeize <63158950+Codeize@users.noreply.github.com>
2020-10-29 23:05:39 +0000

    ..

--- bot.py
+++ bot.py
@@ -1,10 +1,25 @@
 import discord
 from discord.ext import commands
+#import logging // COMING SOON
+#from pathlib import paths
+import json
 
-client = commands.Bot(command_prefix = "-")
+client = commands.Bot(command_prefix = "-", help_command=None)
+
+secret_file = json.load(open(cwd+'/bot_config/secrets.json'))
+bot = commands.Bot(command_prefix='-', case_insensitive=True)
+bot.config_token = secret_file['token']
+#logging.basicConfig(level=logging.INFO)
+
 
 @client.event
 async def on_ready():
-    print('Hyphen is online and active.')
+    print("Hyphen is online and active.")
 
-client.run('NzQ1NjIyMTQyNjU3MzY0MDQw.Xz0cuw.PUkr0_srpyc_ymZ8P-t0WJEwA3c')
+@client.command()
+async def echo(ctx, *, message=None):
+    message = message or "Please provide the message to be echoed!"
+    await ctx.message.delete()
+    await ctx.send(message)
+
+client.run(client.config_token)
